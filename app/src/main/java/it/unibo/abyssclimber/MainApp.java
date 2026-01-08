a876579335bd19b7fe1afb9637134631060b71bb
Matteo Morellini <matteomorellini92@gmail.com>
2026-01-08 18:49:58 +0100

    Sistemato gradle, aggiunti i primi assets, e aggiunto .DS_Store al .gitignore.

---
+++ app/src/main/java/it/unibo/abyssclimber/MainApp.java
@@ -0,0 +1,17 @@
+package it.unibo.abyssclimber;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        // It shows the window
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
